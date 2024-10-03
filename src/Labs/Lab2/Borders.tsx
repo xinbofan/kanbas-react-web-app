36ba82cef79ad4ac3f4c443b7914d1469361a95c
xinbo <fan.xinb@northeastern.edu>
2024-10-02 22:18:03 -0400

    a224100201

---
+++ src/Labs/Lab2/Borders.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.css";
+
+const Borders = () => {
+  return (
+    <div id="wd-css-borders">
+      <h2>Borders</h2>
+      <p className="wd-border-fat wd-border-red wd-border-solid">
+        Solid fat red border
+      </p>
+      <p className="wd-border-thin wd-border-blue wd-border-dashed">
+        Dashed thin blue border
+      </p>
+    </div>
+  );
+};
+
+export default Borders;
