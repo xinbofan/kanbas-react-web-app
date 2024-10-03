e6452fd3521afd15a58fb70602a6b0abfa601e8f
xinbo <fan.xinb@northeastern.edu>
2024-10-03 16:14:34 -0400

    a224100308

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -5,30 +5,30 @@ import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
+import { table } from "console";
+import { FaAlignJustify } from "react-icons/fa";
 
 export default function Courses() {
   return (
     <div id="wd-courses">
-      <h2>Course 1234</h2>
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-4 fs-4 mb-1" />
+        Course 1234{" "}
+      </h2>{" "}
       <hr />
-      <table>
-        <tr>
-          <td valign="top">
-            <CoursesNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/" element={<Navigate to="Home" />} />
-              <Route path="Home" element={<Home />} />
-              <Route path="Modules" element={<Modules />} />
-              <Route path="Assignments" element={<Assignments />} />
-              <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-
-              <Route path="People" element={<h2>People</h2>} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
+      <div className="d-flex">
+        <div className="d-none d-md-block">
+          <CoursesNavigation />
+        </div>
+        <div className="flex-fill">
+          <Routes>
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
+            <Route path="Assignments" element={<Assignments />} />
+            <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,27 +4,22 @@ import React from "react";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
+import "./style.css";
 
 export default function Kanbas() {
   return (
-    <div id="wd-kanbas">
-      <table>
-        <tr>
-          <td valign="top">
-            <KanbasNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/" element={<Navigate to="/Kanbas/Account" />} />
-              <Route path="/Account/*" element={<Account />} />
-              <Route path="/Dashboard" element={<Dashboard />} />
-              <Route path="/Courses/:cid/*" element={<Courses />} />
-              <Route path="/Calendar" element={<h1>Calendar</h1>} />
-              <Route path="/Inbox" element={<h1>Inbox</h1>} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
+    <div id="wd-kanbas" className="wd-main-content-offset p-3">
+      <KanbasNavigation />
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/Kanbas/Account" />} />
+          <Route path="/Account/*" element={<Account />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Courses/:cid/*" element={<Courses />} />
+          <Route path="/Calendar" element={<h1>Calendar</h1>} />
+          <Route path="/Inbox" element={<h1>Inbox</h1>} />
+        </Routes>
+      </div>
     </div>
   );
 }
