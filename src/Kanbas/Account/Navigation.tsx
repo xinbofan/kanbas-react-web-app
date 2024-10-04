3abc2fd06ce64b349693d591ef3e42eaabda29b3
xinbo <fan.xinb@northeastern.edu>
2024-10-03 20:11:39 -0400

    a224100317

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 export default function AccountNavigation() {
   return (
-    <div id="wd-account-navigation">
+    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
       <Link to={`/Kanbas/Account/Signin`}> Signin </Link> <br />
       <Link to={`/Kanbas/Account/Signup`}> Signup </Link> <br />
       <Link to={`/Kanbas/Account/Profile`}> Profile </Link> <br />
