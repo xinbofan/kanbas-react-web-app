fec6ef9ff34c099bdb265bbc093f1680281515b0
xinbo <fan.xinb@northeastern.edu>
2024-10-08 16:38:23 -0400

    a224100801

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -5,6 +5,7 @@ import LessonControlButtons from "../Modules/LessonControlButtons";
 import { CiSearch } from "react-icons/ci";
 import { GoTriangleDown } from "react-icons/go";
 import { IoEllipsisVertical } from "react-icons/io5";
+import AssignmentControlButton from "./AssignmentControlButton";
 
 export default function Assignments() {
   return (
@@ -36,27 +37,28 @@ export default function Assignments() {
             <span className="badge bg-light text-muted px-2 py-1 rounded-pill">
               40% of Total
             </span>
-            <button className="btn btn-link">+</button>
-            <IoEllipsisVertical className="fs-4" />
+            <AssignmentControlButton />
           </div>
           <ul className="wd-assignment-content list-group rounded-0">
             <li className="wd-assignment list-group-item p-3 ps-1">
               <BsGripVertical className="me-2 fs-3" />
               <MdOutlineAssignment className="me-2 fs-3" />
-              <div>
+              <>
                 <a
                   className="wd-assignment-link fw-bold text-dark"
                   href="#/Kanbas/Courses/1234/Assignments/123"
                 >
                   A1
                 </a>
-                <p className="mb-0 text-danger">Multiple Modules</p>
-                <small className="text-muted">
-                  Not available until May 6 at 12:00am | Due May 13 at 11:59pm |
-                  100 pts
-                </small>
+                <p className="mb-0 text-muted">
+                  <span style={{ color: "red" }}>Multiple Modules </span>|{" "}
+                  <span style={{ color: "grey" }}>Not available until </span>May
+                  6 at 12:00am |<span style={{ color: "grey" }}> Due </span>May
+                  13 at 11:59pm | 100 pts
+                </p>
+
                 <LessonControlButtons />
-              </div>
+              </>
             </li>
 
             <li className="wd-assignment list-group-item p-3 ps-1">
