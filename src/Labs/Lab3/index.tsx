afe34de829ab7e5a50c56710fbc23c524b546af4
xinbo <fan.xinb@northeastern.edu>
2024-10-16 16:12:20 -0400

    a324101605

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -33,6 +33,7 @@ import Highlight from "./Highlight";
 import PathParameters from "./PathParameters";
 
 export default function Lab3() {
+  console.log("Hello World!");
   return (
     <div>
       <h2>Lab 3</h2>
