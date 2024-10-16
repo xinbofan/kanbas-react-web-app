347653b466ca41cb791db1a2dc870da2b690e4f6
xinbo <fan.xinb@northeastern.edu>
2024-10-16 15:19:55 -0400

    a324101603

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -25,6 +25,8 @@ import Spreading from "./Spreading";
 import Destructing from "./Destructing";
 import FunctionDestructing from "./FunctionDestructing";
 import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
 
 export default function Lab3() {
   return (
@@ -56,6 +58,8 @@ export default function Lab3() {
       <Destructing />
       <FunctionDestructing />
       <DestructingImports />
+      <Classes />
+      <Styles />
     </div>
   );
 }
