f39bea73f0ca23b5cacba61f987b32a9eba0a56e
xinbo <fan.xinb@northeastern.edu>
2024-10-17 18:11:22 -0400

    a324101706

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CoursesNavigation from "./Navigation";
-import { Navigate, Route, Routes, useParams } from "react-router";
+import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
@@ -13,11 +13,12 @@ import { courses } from "../Database";
 export default function Courses() {
   const { cid } = useParams();
   const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        {course && course.name}
+        {course && course.name} &gt; {pathname.split("/")[4]}
       </h2>{" "}
       <hr />
       <div className="d-flex">
