dbf4bd1c581050463b95c8fc1cf3e3ff2e7bf6b9
xinbo <fan.xinb@northeastern.edu>
2024-10-25 01:38:37 -0400

    a424102504

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,7 +1,20 @@
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+
 export default function Lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
+
   return (
     <div>
       <h2>Lab 4</h2>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
     </div>
   );
 }
