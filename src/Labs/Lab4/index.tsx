15f9fd0db18acc3ea9887fede8f0b09131e20c66
xinbo <fan.xinb@northeastern.edu>
2024-10-25 15:57:40 -0400

    a424102510

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -9,6 +9,7 @@ import DateStateVariable from "./DateStateVariable";
 import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 
 export default function Lab4() {
   function sayHello() {
@@ -29,6 +30,7 @@ export default function Lab4() {
       <ObjectStateVariable />
       <ArrayStateVariable />
       <ParentStateComponent />
+      <ReduxExamples />
     </div>
   );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -5,27 +5,32 @@ import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import Lab4 from "./Lab4";
 import React from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+
 export default function Labs() {
   return (
-    <div>
-      <h2>Xinbo Fan, Section 02</h2>
-      <h1>Labs</h1>
-      <TOC />
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2" element={<Lab2 />} />
-        <Route path="Lab3/*" element={<Lab3 />} />
-        <Route path="Lab4" element={<Lab4 />} />
-      </Routes>
-      <a
-        href="https://github.com/xinbofan/kanbas-react-web-app"
-        target="_blank"
-        rel="noopener noreferrer"
-        id="wd-github"
-      >
-        Relevant source code repositories
-      </a>
-    </div>
+    <Provider store={store}>
+      <div className="container-fluid">
+        <h2>Xinbo Fan, Section 02</h2>
+        <h1>Labs</h1>
+        <TOC />
+        <Routes>
+          <Route path="/" element={<Navigate to="Lab1" />} />
+          <Route path="Lab1" element={<Lab1 />} />
+          <Route path="Lab2" element={<Lab2 />} />
+          <Route path="Lab3/*" element={<Lab3 />} />
+          <Route path="Lab4" element={<Lab4 />} />
+        </Routes>
+        <a
+          href="https://github.com/xinbofan/kanbas-react-web-app"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="wd-github"
+        >
+          Relevant source code repositories
+        </a>
+      </div>
+    </Provider>
   );
 }
