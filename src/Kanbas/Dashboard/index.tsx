e4b186672f28df3cd429378e5dbfcb01179cd613
xinbo <fan.xinb@northeastern.edu>
2024-10-28 21:40:53 -0400

    a424102806

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -70,10 +70,14 @@ export default function Dashboard({
                 <h5 className="card-title">{course.name}</h5>
                 <p className="card-text">{course.description}</p>
 
-                <div className="card-footer d-flex justify-content-between align-items-center">
-                  <button className="btn btn-primary">
-                    <Link to={`/Courses/${course._id}`}>Go</Link>
-                  </button>
+                <div className="d-flex justify-content-between align-items-center">
+                  <Link
+                    to={`/Kanbas/Courses/${course._id}`}
+                    className="btn btn-primary"
+                  >
+                    Go
+                  </Link>
+
                   <div>
                     <button
                       onClick={(event) => {
