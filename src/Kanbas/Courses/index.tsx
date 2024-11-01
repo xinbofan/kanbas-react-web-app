b49f23bed59578f5f2f5e6f60d6096f91156b431
xinbo <fan.xinb@northeastern.edu>
2024-10-31 22:18:05 -0400

    a424103109

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -31,7 +31,7 @@ export default function Courses({ courses }: { courses: any[] }) {
             <Route path="Assignments" element={<Assignments />} />
             <Route path="Assignments/:aid" element={<AssignmentEditor />} />
             <Route path="People" element={<PeopleTable />} />
-            <Route path="new" element={<AssignmentEditor />} />
+            <Route path="Assignments/new" element={<AssignmentEditor />} />
           </Routes>
         </div>
       </div>
