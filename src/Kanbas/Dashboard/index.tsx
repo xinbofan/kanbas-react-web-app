3bb65bd1e410a51d8990e59a92fc2729fe6659ac
xinbo <fan.xinb@northeastern.edu>
2024-11-06 20:10:54 -0500

    a424103140

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -65,18 +65,19 @@ export default function Dashboard() {
             Update
           </button>
         </h5>
+
+        <br />
+        <input
+          defaultValue={currentCourse.name}
+          className="form-control mb-2"
+          onChange={(e) => handleCourseChange("name", e.target.value)}
+        />
+        <textarea
+          defaultValue={currentCourse.description}
+          className="form-control"
+          onChange={(e) => handleCourseChange("description", e.target.value)}
+        />
       </ProtectedFaculty>
-      <br />
-      <input
-        defaultValue={currentCourse.name}
-        className="form-control mb-2"
-        onChange={(e) => handleCourseChange("name", e.target.value)}
-      />
-      <textarea
-        defaultValue={currentCourse.description}
-        className="form-control"
-        onChange={(e) => handleCourseChange("description", e.target.value)}
-      />
       <hr />
       <h2 id="wd-dashboard-published">
         {showAllCourses ? "All Courses" : "Enrolled Courses"} (
@@ -87,96 +88,88 @@ export default function Dashboard() {
         className="row row-cols-1 row-cols-md-5 g-4"
         id="wd-dashboard-courses"
       >
-        {filteredCourses
-          .filter((course: { _id: any }) =>
-            enrollments.some(
-              (enrollment: { user: any; course: any }) =>
-                enrollment.user === currentUser._id &&
-                enrollment.course === course._id
-            )
-          )
-          .map(
-            (course: {
-              _id: React.Key | null | undefined;
-              image: any;
-              name:
-                | string
-                | number
-                | boolean
-                | React.ReactElement<
-                    any,
-                    string | React.JSXElementConstructor<any>
-                  >
-                | Iterable<React.ReactNode>
-                | React.ReactPortal
-                | null
-                | undefined;
-              description:
-                | string
-                | number
-                | boolean
-                | React.ReactElement<
-                    any,
-                    string | React.JSXElementConstructor<any>
-                  >
-                | Iterable<React.ReactNode>
-                | React.ReactPortal
-                | null
-                | undefined;
-            }) => (
-              <div key={course._id} className="col" style={{ width: "300px" }}>
-                <div className="card rounded h-100">
-                  <img
-                    src={course.image || "/images/reactjs.jpg"}
-                    className="card-img-top"
-                    alt="Course"
-                  />
-                  <div className="card-body d-flex flex-column justify-content-between">
-                    <div>
-                      <h5 className="card-title">{course.name}</h5>
-                      <p className="card-text">{course.description}</p>
-                    </div>
-                    <div className="d-flex justify-content-between align-items-center mt-3">
-                      <Link
-                        to={`/Kanbas/Courses/${course._id}/Home`}
-                        className="btn btn-primary"
-                      >
-                        Go
-                      </Link>
-                      <ProtectedFaculty>
-                        <div>
-                          <button
-                            onClick={(event) => {
-                              event.preventDefault();
-                              deleteCourse(course._id);
-                            }}
-                            className="btn btn-danger float-end"
-                            id="wd-delete-course-click"
-                          >
-                            Delete
-                          </button>
+        {filteredCourses.map(
+          (course: {
+            _id: React.Key | null | undefined;
+            image: any;
+            name:
+              | string
+              | number
+              | boolean
+              | React.ReactElement<
+                  any,
+                  string | React.JSXElementConstructor<any>
+                >
+              | Iterable<React.ReactNode>
+              | React.ReactPortal
+              | null
+              | undefined;
+            description:
+              | string
+              | number
+              | boolean
+              | React.ReactElement<
+                  any,
+                  string | React.JSXElementConstructor<any>
+                >
+              | Iterable<React.ReactNode>
+              | React.ReactPortal
+              | null
+              | undefined;
+          }) => (
+            <div key={course._id} className="col" style={{ width: "300px" }}>
+              <div className="card rounded h-100">
+                <img
+                  src={course.image || "/images/reactjs.jpg"}
+                  className="card-img-top"
+                  alt="Course"
+                />
+                <div className="card-body d-flex flex-column justify-content-between">
+                  <div>
+                    <h5 className="card-title">{course.name}</h5>
+                    <p className="card-text">{course.description}</p>
+                  </div>
+                  <div className="d-flex justify-content-between align-items-center mt-3">
+                    <Link
+                      to={`/Kanbas/Courses/${course._id}/Home`}
+                      className="btn btn-primary"
+                    >
+                      Go
+                    </Link>
+                    <ProtectedFaculty>
+                      <div>
+                        <button
+                          onClick={(event) => {
+                            event.preventDefault();
+                            deleteCourse(course._id);
+                          }}
+                          className="btn btn-danger float-end"
+                          id="wd-delete-course-click"
+                        >
+                          Delete
+                        </button>
 
-                          <button
-                            id="wd-edit-course-click"
-                            onClick={(event) => {
-                              event.preventDefault();
-                              handleCourseChange("currentCourse", course);
-                            }}
-                            className="btn btn-warning me-2 float-end"
-                          >
-                            Edit
-                          </button>
-                        </div>
-                      </ProtectedFaculty>
-                      console.log("showAllCourses:", showAllCourses);
-                      console.log("filteredCourses:", filteredCourses);
-                      console.log("enrollments:", enrollments);
-                    </div>
+                        <button
+                          id="wd-edit-course-click"
+                          onClick={(event) => {
+                            event.preventDefault();
+                            handleCourseChange("currentCourse", course);
+                          }}
+                          className="btn btn-warning me-2 float-end"
+                        >
+                          Edit
+                        </button>
+                      </div>
+                    </ProtectedFaculty>
+                    console.log("showAllCourses:", showAllCourses);
+                    console.log("filteredCourses:", filteredCourses);
+                    console.log("enrollments:", enrollments);
                   </div>
                 </div>
               </div>
-            )
-          )}
+            </div>
+          )
+        )}
       </div>
     </div>
   );
